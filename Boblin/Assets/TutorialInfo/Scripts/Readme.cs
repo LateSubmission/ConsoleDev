4a98d5932cdaca870b52b806284fddf41bda4d0a
Rowan Noble <rowan.noble@mail.bcu.ac.uk>
2024-03-08 13:02:51 +0000

    Changes for Linter pt. 2

--- Boblin/Assets/TutorialInfo/Scripts/Readme.cs
+++ Boblin/Assets/TutorialInfo/Scripts/Readme.cs
@@ -7,7 +7,7 @@ public class Readme : ScriptableObject
     public string title;
     public Section[] sections;
     public bool loadedLayout;
-    
+
     [Serializable]
     public class Section
     {
